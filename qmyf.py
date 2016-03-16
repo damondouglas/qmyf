7f08d73f288b1d1c5eca5bdc5c3cb199feaf4553
Damon Douglas <douglas.damon@gmail.com>
2016-03-15 18:29:54 -0700

    add MFA

--- qmyf.py
+++ qmyf.py
@@ -2,6 +2,9 @@
 Usage:
   qmyf.py init <client> <secret> <gpgkeyname> [--gpghomedir=<gpghomedir>] [--gpgbinary=<gpgbinary>] [--plaid-endpoint=<plaidendpoint>]
   qmyf.py auth <inst>
+  qmyf.py mfainit <inst> (phone|email)
+  qmyf.py authmfa <inst> <code>
+  qmyf.py token <inst>
   qmyf.py (inst|privacy|noprompt|prompt)
   qmyf.py q <inst> <search>
   qmyf.py -h | --help | --version
@@ -23,12 +26,16 @@ from plaid import errors as plaid_errors
 def _q(arguments):
     inst = arguments['<inst>']
     search = arguments['<search>']
+    opts = None
+    if search != '.':
+        opts = {'gte': search}
+
     passphrase = _get_gpg_password()
     crypter_util = crypt.Crypt()
     cached_inst = crypter_util.decrypt_json_from_file('%s.gpg' % inst, passphrase)
     access_token = cached_inst['access_token']
     client = _get_plaid_client(passphrase, access_token=access_token)
-    response = client.connect_get()
+    response = client.connect_get(opts=opts)
     data = json.loads(response.content)
     transactions = data['transactions']
     print common.pretty_print_column_names(transactions)
@@ -38,6 +45,14 @@ def _privacy(arguments):
     print """
         Privacy Policy can be read at https://plaid.com/legal.
     """
+
+def _token(arguments):
+    inst = arguments['<inst>']
+    password = _get_gpg_password()
+    crypto_util = crypt.Crypt()
+    inst_config_data = crypto_util.decrypt_json_from_file('%s.gpg' % inst, password)
+    access_token = inst_config_data['access_token']
+    print access_token
 
 def _prompt(arguments):
     yes = set(['yes','y', 'ye', ''])
@@ -101,6 +116,46 @@ def _get_plaid_client(passphrase, access_token = None):
 def _get_gpg_password():
     return common.get_password()
 
+def _mfainit(arguments):
+    inst = arguments['<inst>']
+    method = "email"
+    if arguments['phone']:
+        method = "phone"
+
+    password = _get_gpg_password()
+    crypto_util = crypt.Crypt()
+    inst_config_data = crypto_util.decrypt_json_from_file('%s.gpg' % inst, password)
+    access_token = inst_config_data['access_token']
+    client = _get_plaid_client(password, access_token=access_token)
+    client.connect_step(inst, None, options={
+        'send_method': {'type': method}
+    })
+    print "MFA request sent to %s. Institution will send authorization code to %s. Run:" % (inst, method)
+    print "$ qmyf.py authmfa %s <code>" % inst
+    print "to finalize MFA authorization."
+
+def _authmfa(arguments):
+    inst = arguments['<inst>']
+    code = arguments['<code>']
+    password = _get_gpg_password()
+    crypto_util = crypt.Crypt()
+    inst_config_data = crypto_util.decrypt_json_from_file('%s.gpg' % inst, password)
+    access_token = inst_config_data['access_token']
+    client = _get_plaid_client(password, access_token=access_token)
+    # username = raw_input("%s username: " % inst)
+    # password = getpass.getpass("%s password: " % inst)
+    if client and inst and code:
+        try:
+            # response = client.connect(inst, {
+            #     'username': username,
+            #     'password': password
+            # })
+            mfa_response = client.connect_step(inst, code)
+            print mfa_response.content
+
+        except plaid_errors.UnauthorizedError as e:
+            print e
+
 def _auth(arguments):
     _check_if_init()
     passphrase = _get_gpg_password()
@@ -125,6 +180,11 @@ def _auth(arguments):
                         'access_token': access_token,
                         'inst': inst
                     }
+
+                    if data['type'] and data['type'] == 'list':
+                        print "MFA authorization needed. Run: "
+                        print "$ qmyf.py mfainit %s (phone|email)" % inst
+                        print "to initiate MFA authorization."
 
                     crypter_util.encrypt_json_to_file(auth_data, "%s.gpg" % inst)
                     print "institution authorization stored in %s.gpg" % inst
